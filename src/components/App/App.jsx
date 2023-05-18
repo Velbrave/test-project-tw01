25e3af0d03d8287644e65b2c9aeddda002e2e566
Vitalij Nesvyatipaska <Parkovaja60@gmail.com>
2023-05-18 14:37:06 +0300

    lazy

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,10 @@
+import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from '../../page/HomePage/HomePage';
-import UpdatePage from '../../page/UpdatePage/UpdatePage';
-import NotFound from '../NotFound/NotFound';
 import { Layout } from '../Layout/Layout';
+import NotFound from '../NotFound/NotFound';
+
+const HomePage = lazy(() => import('../../page/HomePage/HomePage'));
+const UpdatePage = lazy(() => import('../../page/UpdatePage/UpdatePage'));
 
 function App() {
   return (
